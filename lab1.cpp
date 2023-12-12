d610a9cf872125f5152433ef99508537d5da4446
Dan Omelyanyuk <omelyanyukworking@gmail.com>
2023-12-12 10:31:43 +0700

    lab1

--- lab1.cpp
+++ lab1.cpp
@@ -4,9 +4,9 @@
 
 #define NUM_EVENTS 5
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond_producer = PTHREAD_COND_INITIALIZER;
-pthread_cond_t cond_consumer = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t *mutex = new PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t *cond_producer = new PTHREAD_COND_INITIALIZER;
+pthread_cond_t *cond_consumer = new PTHREAD_COND_INITIALIZER;
 
 volatile int events[NUM_EVENTS] = { 0 };
 
@@ -15,16 +15,16 @@ void *producerThread(void *arg) {
     for (int i = 0; i < NUM_EVENTS; ++i) {
         sleep(1); 
 
-        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(mutex);
         
         while (events[i]) 
-            pthread_cond_wait(&cond_producer, &mutex);
+            pthread_cond_wait(cond_producer, mutex);
 
         events[i] = 1;
         printf("Event %d Produced.\n", i + 1);
         
-        pthread_cond_signal(&cond_consumer);
-        pthread_mutex_unlock(&mutex);
+        pthread_cond_signal(cond_consumer);
+        pthread_mutex_unlock(mutex);
     }
 
     return nullptr;
@@ -32,17 +32,17 @@ void *producerThread(void *arg) {
 
 void *consumerThread(void *arg) {
     for (int i = 0; i < NUM_EVENTS; ++i) {
-        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(mutex);
         
         while (!events[i]) 
-            pthread_cond_wait(&cond_consumer, &mutex);
+            pthread_cond_wait(cond_consumer, mutex);
 
         printf("Event %d Consumed.\n\n", i + 1);
         events[i] = 0;
 
-        pthread_cond_signal(&cond_producer);
+        pthread_cond_signal(cond_producer);
         
-		pthread_mutex_unlock(&mutex);
+		pthread_mutex_unlock(mutex);
     }
 
     return nullptr;
